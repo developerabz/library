fe49475b574268cb43a708a55d6404477d8deab3
Abdullah Yakub <abdullah.yakub@hotmail.com>
2022-06-04 13:33:36 +1000

    create button that allows to add each book item by title and author

--- script.js
+++ script.js
@@ -1,16 +1,45 @@
-let myLibrary = ["Hobbit by Tolkien", "Don Quixote by Cervantes"];
+let myLibrary = [];
 
 const cards = document.querySelector(".cards");
 
+
+function ultimate() {
+
+}
+
+ultimate.prototype.addBook = function() {
+    myLibrary.push(`${this.title} by ${this.author}`)
+}
 
 function Book(title, author) {
     //the constructor
     this.title = title
     this.author = author
 }
-const work = new Book("Work", "Me");
 
-console.log(addBookToLibrary());
+Book.prototype = Object.create(ultimate.prototype)
+
+
+
+const button = document.querySelector(".add");
+
+function addedBook() {
+    let work = new Book(prompt("Title: ", ""), prompt("Author: ", ""));
+    return work.addBook();
+}
+
+button.addEventListener("click", function() {
+    addedBook();
+
+    myLibrary = [...new Set(myLibrary)];
+    array();
+    
+    
+});
+
+
+
+/*console.log(addBookToLibrary());
 
 function addBookToLibrary() {
     //do stuff here adds book to library using button
@@ -21,15 +50,32 @@ function addBookToLibrary() {
 
 
 addBookToLibrary();
+*/
+
 
 function array() {
-
-   // let result = "";
-    for (const piece of myLibrary) {
+    //for (let i = 0; i < myLibrary.length; i++) {
+        
         const card = document.createElement('div');
-        card.textContent = `${piece}`;
+        card.classList.add("card");
+        card.textContent = `${myLibrary[myLibrary.length - 1]}`;
+        const btn = document.createElement("button");
+        btn.classList.add("btn");
+        btn.setAttribute("id", `${myLibrary[myLibrary.length - 1]}`);
+        btn.textContent = `Delete`;
+        card.appendChild(btn);
         cards.appendChild(card);
-    }
+    //}
+    
 }
 
-array();
+if (button.classList.contains("btn")) {
+    const btns = document.querySelector(".btn");
+    btns.addEventListener("click", function() {
+        console.log("sfds");
+    //need to make clear button
+    //then all that is left is to style and
+    //format the page to look nicer
+});
+}
+
