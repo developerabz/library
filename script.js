ba3362fef504937472e4d34a740a908edeaedc32
Abdullah Yakub <abdullah.yakub@hotmail.com>
2022-06-26 22:43:04 +1000

    fix js file to add objects with loop

--- script.js
+++ script.js
@@ -1,81 +1,50 @@
-let myLibrary = [];
+let myLibrary = []
 
-const cards = document.querySelector(".cards");
-
-
-function ultimate() {
-
+function Book() {
+    let book = new Object({
+        title: 'Test Book',
+        author: 'Test Author'
+    })
+    return book
 }
 
-ultimate.prototype.addBook = function() {
-    myLibrary.push(`${this.title} by ${this.author}`)
-}
 
-function Book(title, author) {
-    //the constructor
-    this.title = title
-    this.author = author
-}
-
-Book.prototype = Object.create(ultimate.prototype)
-
-
-
-const button = document.querySelector(".add");
-
-function addedBook() {
-    let work = new Book(prompt("Title: ", ""), prompt("Author: ", ""));
-    return work.addBook();
-}
-
-button.addEventListener("click", function() {
-    addedBook();
-
-    myLibrary = [...new Set(myLibrary)];
-    array();
-    
-    
-});
-
-
-
-/*console.log(addBookToLibrary());
 
 function addBookToLibrary() {
-    //do stuff here adds book to library using button
-    myLibrary.push(`${Book.title} by ${Book.author}`)
-}
-
-
-
-
-addBookToLibrary();
-*/
-
-
-function array() {
-    //for (let i = 0; i < myLibrary.length; i++) {
+    
+    const add = document.querySelector('.add')
+    add.addEventListener('click', () => {
+        myLibrary.push({
+            title: prompt('What is the Book?',''),
+            author: prompt('Who is the author?','')
+        })
+        console.log(myLibrary)
         
-        const card = document.createElement('div');
-        card.classList.add("card");
-        card.textContent = `${myLibrary[myLibrary.length - 1]}`;
-        const btn = document.createElement("button");
-        btn.classList.add("btn");
-        btn.setAttribute("id", `${myLibrary[myLibrary.length - 1]}`);
-        btn.textContent = `Delete`;
-        card.appendChild(btn);
-        cards.appendChild(card);
-    //}
+        
+    })
+    myLibrary.push(Book())
+    // addBookToScreen()
+    
+    
     
 }
 
-if (button.classList.contains("btn")) {
-    const btns = document.querySelector(".btn");
-    btns.addEventListener("click", function() {
-        console.log("sfds");
-    //need to make clear button
-    //then all that is left is to style and
-    //format the page to look nicer
-});
+function addBookToScreen() {
+    const loopy = myLibrary.forEach(libraryBook => {
+        const cards = document.querySelector('.cards')
+        const card = document.createElement('div')
+        card.classList.add('card')
+        card.textContent = `${libraryBook.title} by ${libraryBook.author}`
+        const btn = document.createElement('button')
+        btn.classList.add('btn')
+        btn.setAttribute('data-index-number', `${libraryBook.title}`)
+        btn.textContent = 'Delete'
+        card.appendChild(btn)
+        cards.appendChild(card)
+        console.log(cards)
+    })
+
+    
 }
 
+addBookToLibrary()
