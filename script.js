5efe069c54d9e5daf1a8641cd9c1538c2486bdcc
Abdullah Yakub <abdullah.yakub@hotmail.com>
2022-08-09 20:56:09 +1000

    add delete icon instead of text, restructure layout of card

--- script.js
+++ script.js
@@ -1,14 +1,9 @@
+// initialize items in library and idCounter for
+// each unique library book
 let myLibrary = []
 let idCounter = 0;
 
-// function Book(title, author, pages, readCompleted) {
-//     this.title = title;
-//     this.author = author;
-//     this.pages = pages;
-//     this.readCompleted = readCompleted;
-//     this.id = idCounter++;
-// }
-
+// class constructor of book objexts
 class Book {
     constructor (title, author, pages, readCompleted) {
         this.title = title;
@@ -20,7 +15,7 @@ class Book {
 }
 
 function addBookToLibrary() {
-    
+    // adds book object to library array
     const add = document.querySelector('.add')
     add.addEventListener('click', function(ev) {
         ev.preventDefault();
@@ -31,9 +26,8 @@ function addBookToLibrary() {
         const newBook = new Book(newBookTitle, newBookAuthor, newBookPages,
             newBookCompleted);
         myLibrary.push(newBook);
-        console.log(myLibrary)
         
-
+        // adds book as a card to the html display
         const cards = document.querySelector('.cards');
         for (i = 0; i < myLibrary.length; i++) {
             if (i == newBook.id) {
@@ -41,31 +35,46 @@ function addBookToLibrary() {
                 card.classList.add('card');
 
                 const cardTitleText = document.createElement('p');
-                cardTitleText.textContent = `Title: ${myLibrary[i].title}`;
+                cardTitleText.textContent = `'${myLibrary[i].title}' by '${myLibrary[i].author}'`;
 
-                const cardAuthorText = document.createElement('p');
-                cardAuthorText.textContent = `Author: ${myLibrary[i].author}`;
+                // const cardAuthorText = document.createElement('p');
+                // cardAuthorText.textContent = `Author: ${myLibrary[i].author}`;
 
                 const cardPageRange = document.createElement('p');
-                cardPageRange.textContent = `Pages: ${myLibrary[i].pages}`;
+                cardPageRange.textContent = `${myLibrary[i].pages} pages read`;
+
+                const buttonDiv = document.createElement('div');
 
                 const cardDeleteButton = document.createElement('button');
+                cardDeleteButton.classList.add('deleter');
                 cardDeleteButton.dataset.idNumber = newBook.id;
-                cardDeleteButton.textContent = 'Delete';
+                cardDeleteButton.textContent = '';
+
+                const deleteButtonImg = document.createElement('img');
+                deleteButtonImg.dataset.idNumber = newBook.id;
+                deleteButtonImg.setAttribute('src', 'images/delete-icon.png')
+                deleteButtonImg.setAttribute('alt', 'delete button')
 
                 const cardReadButton = document.createElement('button');
                 // cardDeleteButton.dataset.idNumber = newBook.id;
                 cardReadButton.textContent = newBook.readCompleted ? 'Read' : 'Not Read';
 
+                cardDeleteButton.appendChild(deleteButtonImg)
+                buttonDiv.appendChild(cardDeleteButton);
+                buttonDiv.appendChild(cardReadButton);
                 card.appendChild(cardTitleText);
-                card.appendChild(cardAuthorText);
+                // card.appendChild(cardAuthorText);
                 card.appendChild(cardPageRange);
-                card.appendChild(cardDeleteButton);
-                card.appendChild(cardReadButton);
+                card.appendChild(buttonDiv);
                 cards.appendChild(card);
             }  
         }
-        console.log(newBook.id)
+        
+        // emptys the values of the input in html
+        document.getElementById('title').value = '';
+        document.getElementById('author').value = '';
+        document.getElementById('pages').value = '';
+        document.getElementById('readstat').checked = false;
     });
     
     
@@ -73,7 +82,7 @@ function addBookToLibrary() {
 
 function removeBookFromLibrary() {
     const remove = document.querySelector('.cards')
-
+    // creates remove button for each book
     remove.addEventListener('click', (ev) => {
         
         myLibrary.forEach(book => {
@@ -83,16 +92,24 @@ function removeBookFromLibrary() {
             } 
         })
         
-        if (ev.target.textContent == 'Delete') {
-            const card = ev.target.parentElement;
+        if (ev.target.class == '.deleter') {
+            const buttonDiv = ev.target.parentElement;
+            const card = buttonDiv.parentElement;
+
             card.parentElement.removeChild(card);    
+        } else if (ev.target.alt == 'delete button') {
+            const cardDeleteButton = ev.target.parentElement;
+            const buttonDiv = cardDeleteButton.parentElement;
+            const card = buttonDiv.parentElement;
+
+            card.parentElement.removeChild(card); 
         }
     });
 }
 
 function readStatus() {
     const read = document.querySelector('.cards');
-
+    // creates read or not read button for each  book
     read.addEventListener('click', (ev) => {
         myLibrary.forEach(book => {
                 book.readCompleted = !book.readCompleted;
@@ -103,14 +120,17 @@ function readStatus() {
 
 const addBook = document.querySelector('.addbook');
 
+
+// button to remove form from css
 addBook.addEventListener('click', () => {
+    
     const form = document.getElementById('bookmaker');
-    if (form.style.display === "none") {
-        form.style.display = "block"
-        addBook.textContent = "Hide Book Form"
-    } else {
+    if (form.style.display === "block") {
         form.style.display = "none"
         addBook.textContent = "Add Book"
+    } else {
+        form.style.display = "block"
+        addBook.textContent = "Hide Book Form"
     }
     
 })
